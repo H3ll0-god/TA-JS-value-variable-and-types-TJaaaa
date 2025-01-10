16489f70f6ffbb5715d96fa37743661acc8b433a
H3ll0-god <sabbanirohan@gmail.com>
2025-01-10 16:40:39 -0500

    Updated the String template iteral index.js

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -2,7 +2,10 @@
 
 /* Do the following:
 - Define a variable named `username` using `const` keyword and assign your name as a value.
+const username =  "RohanSabbani";
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
+let bio = `Learning to code`;
+
 */
 
 let firstName = "Arya";
@@ -10,12 +13,19 @@ let lastName = "Stark";
 
 /* Do the following using `+` operator
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
+
+  alert("I am " + username + " and my bio is " + bio );
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
+  
+  alert("I\'m " + firstName + " " + lastName + of Winterfell"
 */
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
+  alert("I am" ${username} and my bio is ${bio}" );. Replace square brackets with the value of variable
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
+  alert(`I\'m ${firstName} ${lastName} of Winterfell`);
+  
 */
 
 let amount = 200;
@@ -24,6 +34,11 @@ const taxPercent = 10;
 /* Use string template literal:
   - Alert a message saying `The total amount is [amount]
     the tax amount is [(amount * taxPercent) / 100]
+    
+    let taxamount = ( amount * taxPercent ) / 100;
+    
+    alert(`The total amount is ${amount}
+    the tax amount is ${taxamount}`);
   `
   - Above message should be in two lines
 */
@@ -31,6 +46,8 @@ const taxPercent = 10;
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
     the tax amount is [(amount * taxPercent) / 100]
+alert("The total amount is " + amount + "\n" + 
+      "The tax amount is " + taxAmount);    
   `
   - Above message should be in two lines
 */
